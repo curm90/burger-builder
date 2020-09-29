de16056290bdc3528d3a2a012add39b91769e6ea
Liam Sutton <devliam90@gmail.com>
2020-09-29 16:53:24 +0100

    add build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,24 @@ import Ingredient from './Ingredient/Ingredient';
 import styles from './Burger.module.css';
 
 const Burger = (props) => {
+  let tranformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Ingredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (tranformedIngredients.length === 0) {
+    tranformedIngredients = <p>Please start adding ingredients</p>;
+  }
+
   return (
     <div className={styles.Burger}>
       <Ingredient type='bread-top' />
-      <Ingredient type='meat' />
-      <Ingredient type='cheese' />
+      {tranformedIngredients}
       <Ingredient type='bread-bottom' />
     </div>
   );
